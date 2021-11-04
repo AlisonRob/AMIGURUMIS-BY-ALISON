0e4ab115895aa9749986ed8a42563c9fa9b68589
AlisonRob <jezitaroberts@gmail.com>
2021-11-03 22:44:12 -0300

    componentes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ContentsComponent } from './components/contents/contents.component';
+import { HomeComponent } from './components/home/home.component';
+import { CarruselComponent } from './components/carrusel/carrusel.component';
+import { GaleriaComponent } from './components/galeria/galeria.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentsComponent,
+    HomeComponent,
+    CarruselComponent,
+    GaleriaComponent,
+    ContactoComponent
   ],
   imports: [
     BrowserModule
